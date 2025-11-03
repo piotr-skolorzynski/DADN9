f361609b18dcba96ebf54f188e2aa6a0d0c227f7
piotr-skolorzynski <p.skolorzynski@gmail.com>
2025-11-03 22:31:14 +0100

    Create UserDto Modify AccountController to return UserDto with token

--- API/Controllers/AccountController.cs
+++ API/Controllers/AccountController.cs
@@ -3,15 +3,16 @@ using System.Text;
 using API.Data;
 using API.DTOs;
 using API.Entities;
+using API.Interfaces;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
 
 namespace API.Controllers;
 
-public class AccountController(AppDbContext context) : BaseApiController
+public class AccountController(AppDbContext context, ITokenService tokenService) : BaseApiController
 {
     [HttpPost("register")] // api/account/register
-    public async Task<ActionResult<AppUser>> Register(RegisterDto registerDto)
+    public async Task<ActionResult<UserDto>> Register(RegisterDto registerDto)
     {
         if (await EmailExists(registerDto.Email)) return BadRequest("Email taken");
 
@@ -28,11 +29,17 @@ public class AccountController(AppDbContext context) : BaseApiController
         context.Users.Add(user);
         await context.SaveChangesAsync();
 
-        return user;
+        return new UserDto
+        {
+            Id = user.Id,
+            Email = user.Email,
+            DisplayName = user.DisplayName,
+            Token = tokenService.CreateToken(user)
+        };
     }
 
     [HttpPost("login")]
-    public async Task<ActionResult<AppUser>> Login(LoginDto loginDto)
+    public async Task<ActionResult<UserDto>> Login(LoginDto loginDto)
     {
         var user = await context.Users.SingleOrDefaultAsync(user => user.Email == loginDto.Email);
 
@@ -47,7 +54,13 @@ public class AccountController(AppDbContext context) : BaseApiController
             if (computedHash[i] != user.Password[i]) return Unauthorized("Inalid password"); //to do change to more generic
         }
 
-        return user;
+        return new UserDto
+        {
+            Id = user.Id,
+            Email = user.Email,
+            DisplayName = user.DisplayName,
+            Token = tokenService.CreateToken(user)
+        };
     }
 
     private async Task<bool> EmailExists(string email)
