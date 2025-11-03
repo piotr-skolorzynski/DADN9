1a7f91e662371f872e1b0042c32a01f35c34491b
piotr-skolorzynski <p.skolorzynski@gmail.com>
2025-11-03 14:24:41 +0100

    Update the user entity preparing to implement basic auth

--- API/Entities/AppUser.cs
+++ API/Entities/AppUser.cs
@@ -5,4 +5,7 @@ public class AppUser
     public string Id { get; set; } = Guid.NewGuid().ToString();
     public required string DisplayName { get; set; }
     public required string Email { get; set; }
+    //hash and salt password, temp solution
+    public required byte[] Password { get; set; }
+    public required byte[] PasswordSalt { get; set; }
 }
