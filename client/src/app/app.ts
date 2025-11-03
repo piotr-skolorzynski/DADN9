a7254171df019993e2b888723064ef95e520bfc2
piotr-skolorzynski <p.skolorzynski@gmail.com>
2025-11-03 14:07:29 +0100

    Initial commit for frontend

---
+++ client/src/app/app.ts
@@ -0,0 +1,25 @@
+import { Component, inject, OnInit, signal } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+
+interface IAppUser {
+  id: string;
+  displayName: string;
+  email: string;
+}
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.html',
+})
+export class App implements OnInit{
+  private http = inject(HttpClient);
+  protected readonly title = signal('Dating app');
+  protected members = signal<IAppUser[]>([]);
+  
+  public ngOnInit(): void {
+    this.http.get<IAppUser[]>('https://localhost:5001/api/members').subscribe({
+      next: response => this.members.set(response), 
+      error: error => console.log(error)
+    });
+  }
+}
