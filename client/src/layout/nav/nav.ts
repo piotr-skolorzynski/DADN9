a7734f7bea35db25323db8da30cf88783399059c
piotr-skolorzynski <p.skolorzynski@gmail.com>
2025-11-04 22:18:37 +0100

    Modify Nav component to use signal based form to login the user

--- client/src/layout/nav/nav.ts
+++ client/src/layout/nav/nav.ts
@@ -1,8 +1,31 @@
-import { Component } from '@angular/core';
+import { Component, signal } from '@angular/core';
+import { Control, email, form, required } from '@angular/forms/signals';
+
+interface LoginForm {
+  email: string;
+  password: string;
+}
 
 @Component({
   selector: 'app-nav',
-  imports: [],
+  imports: [Control],
   templateUrl: './nav.html',
 })
-export class Nav {}
+export class Nav {
+  protected readonly model = signal<LoginForm>({
+    email: '',
+    password: '',
+  });
+
+  protected readonly form = form(this.model, schema => {
+    required(schema.email, {
+      message: 'Your Email is required!',
+    });
+    required(schema.password, {
+      message: 'Your password is required!',
+    });
+    email(schema.email, {
+      message: 'Please enter a valid email address!',
+    });
+  });
+}
