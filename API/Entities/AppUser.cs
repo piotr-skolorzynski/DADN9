8a46d216cc1064fcf27f3841531dd4abbb65e1d1
piotr-skolorzynski <p.skolorzynski@gmail.com>
2025-11-14 11:53:51 +0100

    API Extend User entities by adding Member and Photo models

--- API/Entities/AppUser.cs
+++ API/Entities/AppUser.cs
@@ -6,6 +6,7 @@ public class AppUser
     public required string DisplayName { get; set; }
     public required string Email { get; set; }
     //hash and salt password, temp solution
+    public string? ImageUrl { get; set; }
     public required byte[] Password { get; set; }
     public required byte[] PasswordSalt { get; set; }
 }
---
+++ API/Entities/Photo.cs
@@ -0,0 +1,12 @@
+namespace API.Entities;
+
+public class Photo
+{
+    public int Id { get; set; } 
+    public required string ImageUrl { get; set; }
+    //for cloud solutions
+    public string? PublicId { get; set; }
+
+    //Navigation property
+    public Member Member { get; set; } = null!;
+}
