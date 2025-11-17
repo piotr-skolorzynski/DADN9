5ffb63bab52193ccd50280c644127573bf368181
piotr-skolorzynski <p.skolorzynski@gmail.com>
2025-11-17 15:15:53 +0100

    Client Create Member types Adjust User img src  API adjust AppUserExtensions to return imageUrl

--- client/src/app/models/interfaces/index.ts
+++ client/src/app/models/interfaces/index.ts
@@ -1,4 +1,6 @@
 export * from './api-error.interface';
 export * from './login-credentials.interface';
+export * from './member.interface';
+export * from './photo.interface';
 export * from './register-credentials.interface';
 export * from './user.interface';
---
+++ client/src/app/models/interfaces/photo.interface.ts
@@ -0,0 +1,6 @@
+export interface IPhoto {
+  id: number;
+  url: string;
+  publicId?: string;
+  memberId: string;
+}
