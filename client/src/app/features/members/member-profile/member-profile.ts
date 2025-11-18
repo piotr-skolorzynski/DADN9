e775e2352d92a9d699c7ca893bbe790b37384fe7
piotr-skolorzynski <p.skolorzynski@gmail.com>
2025-11-18 22:38:34 +0100

    Client Use date pipe

--- client/src/app/features/members/member-profile/member-profile.ts
+++ client/src/app/features/members/member-profile/member-profile.ts
@@ -2,10 +2,12 @@ import { Component, inject, OnInit, signal } from '@angular/core';
 import { ActivatedRoute } from '@angular/router';
 import { tap } from 'rxjs';
 import { IMember } from '@models/interfaces';
+import { DatePipe } from '@angular/common';
 
 @Component({
   selector: 'app-member-profile',
   templateUrl: './member-profile.html',
+  imports: [DatePipe],
 })
 export class MemberProfile implements OnInit {
   private readonly route = inject(ActivatedRoute);
